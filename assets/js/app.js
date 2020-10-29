7ceea3bb8b51d7cbf543f76b98c9e23c45f33f3e
Ben Pharn <benjaminpharn@gmail.com>
2020-10-28 22:41:08 -0700

    map states

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,15 @@ d3.csv("assets/data/data.csv").then( healthData => {
   console.log(healthData);
 
   // log a list of names
-  var names = healthData.map(data => data.name);
-  console.log("names", names);
+  var states = healthData.map(data => data.state);
+  console.log("states", states);
 
   // Cast each hours value in tvData as a number using the unary + operator
-  healthData.forEach(function(data) {
-    data.hours = +data.hours;
-    console.log("Name:", data.name);
-    console.log("Hours:", data.hours);
-  });
+  // healthData.forEach( data => {
+  //   data.hours = +data.hours;
+  //   console.log("Name:", data.name);
+  //   console.log("Hours:", data.hours);
+  // });
 }).catch(function(error) {
   console.log(error);
 });
