2b6d1ff4c988083df986fe6653e8fcb078dd4492
Ben Pharn <benjaminpharn@gmail.com>
2020-10-28 22:38:29 -0700

    correct filepath

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,18 @@
 // Load data from hours-of-tv-watched.csv
-d3.csv("./data/data.csv").then( healthData => {
+d3.csv("assets/data/data.csv").then( healthData => {
 
-    console.log(healthData);
-  
-    // log a list of names
-    var names = healthData.map(data => data.name);
-    console.log("names", names);
-  
-    // Cast each hours value in tvData as a number using the unary + operator
-    healthData.forEach(function(data) {
-      data.hours = +data.hours;
-      console.log("Name:", data.name);
-      console.log("Hours:", data.hours);
-    });
-  }).catch(function(error) {
-    console.log(error);
+  console.log(healthData);
+
+  // log a list of names
+  var names = healthData.map(data => data.name);
+  console.log("names", names);
+
+  // Cast each hours value in tvData as a number using the unary + operator
+  healthData.forEach(function(data) {
+    data.hours = +data.hours;
+    console.log("Name:", data.name);
+    console.log("Hours:", data.hours);
   });
-  
+}).catch(function(error) {
+  console.log(error);
+});
