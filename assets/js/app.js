8a2dc073a0f23229668ad3eb95b96a27c43e4bc4
Ben Pharn <benjaminpharn@gmail.com>
2020-11-09 13:55:54 -0800

    change import data source

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,18 @@
-// Load data from hours-of-tv-watched.csv
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -12,7 +21,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("hairData.csv").then(function(hairData) {
+d3.csv("assets/data/data.csv").then( healthData => {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
