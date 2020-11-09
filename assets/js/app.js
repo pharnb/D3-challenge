5912efa15b5bb3e90567f2fef4d4ad225078535d
Ben Pharn <benjaminpharn@gmail.com>
2020-11-09 13:17:45 -0800

    rename states

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,10 @@ d3.csv("assets/data/data.csv").then( healthData => {
   console.log(healthData);
 
   // log a list of names
-  var states = healthData.map(data => data.state);
+  var stateabbr = healthData.map(data => data.abbr);
   var poverty = healthData.map(data => data.poverty);
   var income = healthData.map(data => data.income);
-  console.log("states", states);
+  console.log("states", stateabbr);
   console.log("poverty", poverty);
   console.log("income",income);
 
